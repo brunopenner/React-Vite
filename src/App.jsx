b6c34849d73f53e35b4ee0e04752713acaf53f1d
Bruno Penner <brunopenner01@gmail.com>
2024-10-24 16:58:22 +0800

    #621. Non-String Props

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import './App.css'
 import Chicken from './Chicken'
 import Greeter from './Greeter'
+import Die from './Die'
 
 function App() {
 
   return (
     <div>
-      <Greeter person="Bill"/>
-      <Greeter person="Bruno"/>
-      <Greeter person="Arthur"/>
+      {/* <Greeter person="Bill" from="Colt"/>
+      <Greeter person="Bruno" from="Colt"/>
+      <Greeter person="Arthur" from="Elton"/> */}
+      <Die numSides={20}/>
+      <Die numSides={6}/>
+      <Die numSides={10}/>
     </div>
   )
 }
