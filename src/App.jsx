e9636124cf6da019566e6a39af571a1eb382d403
Bruno Penner <brunopenner01@gmail.com>
2024-10-25 09:14:59 +0800

    #623. Passing Arrays and Objects

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,20 @@ import './App.css'
 import Chicken from './Chicken'
 import Greeter from './Greeter'
 import Die from './Die'
+import ListPicker from './ListPicker'
 
 function App() {
 
   return (
     <div>
-      <Greeter person="Bill" from="Colt"/>
+      {/* <Greeter person="Bill" from="Colt"/>
       <Greeter from="Colt"/>
       <Greeter person="Arthur"/>
       <Die numSides={20}/>
       <Die numSides={6}/>
-      <Die numSides={10}/>
+      <Die numSides={10}/> */}
+      <ListPicker values={[1,2,3]} />
+      <ListPicker values={["a","b","c"]} />
     </div>
   )
 }
