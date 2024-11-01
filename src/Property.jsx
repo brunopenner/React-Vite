d3f2bbb8aadc7a3cebb88ad1fe5fac0b2f3ecb40
Bruno Penner <brunopenner01@gmail.com>
2024-11-01 09:34:22 +0800

    #632. Rental Property Exercise - Solution

--- src/Property.jsx
+++ src/Property.jsx
@@ -1,3 +1,16 @@
-export default function Property() {
-    
+export default function Property({name, price, rating}) {
+    const styles={
+        border: "1px solid black",
+        boxShadow: "2px 2px 10px black",
+        background: "white",
+        padding: "15px",
+        color: "black"
+    }
+    return (
+        <div className="propertyCard" style={styles}>
+            <h2>{name}</h2>
+            <h3>${price} a night</h3>
+            <h4>{rating} ⭐</h4>
+        </div>
+    )
 }
--- src/PropertyList.jsx
+++ src/PropertyList.jsx
@@ -1,3 +1,13 @@
-export default function PropertyList() {
-    
+import Property from "./Property";
+
+export default function PropertyList({properties}) {
+    // const styles= {display: "flex"}
+    return (
+        <div style={{display: "flex"}}>
+            {properties.map((i) => (
+                <Property key={i.id} name={i.name} price={i.price} rating={i.rating}/>
+                // <Property key={i.id} {...i}/>
+            ))}
+        </div>
+    )
 }
