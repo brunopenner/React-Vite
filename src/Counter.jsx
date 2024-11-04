13a2ae25119e6025c6a2639bff53cc9deb99c48d
Bruno Penner <brunopenner01@gmail.com>
2024-11-04 10:03:30 +0800

    #641. Working With The useState() Hook

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,14 +1,28 @@
+import {useState} from "react";
 export default function Counter() {
-    let num = 0;
-    const incrementNum = () => {
-        num+=1;
-        console.log(num);
+    const [num, setNum] = useState(5);
+    const changeNum = () => {
+        setNum(num+1);
     }
-
     return (
         <div>
             <p>The count is: {num}</p>
-            <button onClick={incrementNum}>Increment</button>
+            <button onClick={changeNum}>Increment</button>
         </div>
     )
 }
+
+// export default function Counter() {
+//     let num = 0;
+//     const incrementNum = () => {
+//         num+=1;
+//         console.log(num);
+//     }
+
+//     return (
+//         <div>
+//             <p>The count is: {num}</p>
+//             <button onClick={incrementNum}>Increment</button>
+//         </div>
+//     )
+// }
